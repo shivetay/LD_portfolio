3a39d507e90cae2975b656c865fc305d5cea69d3
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-18 20:01:01 +0100

    add styles to header, small global changes

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './Header.scss';
+
 export default function Header() {
   return (
-    <div>
-      <div>
-        <h3>Łukasz Dawidowicz</h3>
-        <p>Front-End Developer</p>
+    <div className='Header'>
+      <div className='Header__Logo'>
+        <h3 className='Header__Logo-h3'>Łukasz Dawidowicz</h3>
+        <p className='Header__Logo-paragraph'>Front-End Developer</p>
       </div>
-      <div>
+      <div className='Header__Nav'>
         <NavLink to='/'>
           <span>Home</span>
         </NavLink>
