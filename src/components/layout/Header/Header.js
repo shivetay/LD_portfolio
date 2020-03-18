f82972455ce5a513a23e38987b0266b5483d18ff
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-18 14:05:55 +0100

    add navlinks

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
-      <nav>header</nav>
+      <div>
+        <h3>Łukasz Dawidowicz</h3>
+        <p>Front-End Developer</p>
+      </div>
+      <div>
+        <NavLink to='/'>
+          <span>Home</span>
+        </NavLink>
+        <NavLink to='/projects'>
+          <span>Projects</span>
+        </NavLink>
+        <NavLink to='/skills'>
+          <span>Skills</span>
+        </NavLink>
+        <NavLink to='/about'>
+          <span>About Me</span>
+        </NavLink>
+        <NavLink to='/contact'>
+          <span>Contact</span>
+        </NavLink>
+      </div>
     </div>
   );
 }
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import '../../../styles/normalize.scss';
+import '../../../styles/global.scss';
+import '../../../styles/mixins.scss';
+
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
