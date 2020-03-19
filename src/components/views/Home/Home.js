5a39f40e1b47f86eb832bfc349e2ecbb63d8f949
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-19 14:51:53 +0100

    add home basic stylesv and design

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Home extends Component {
+import './Home.scss';
+
+class Home extends Component {
   render() {
     return (
-      <div>
-        <h1> Home component</h1>
+      <div className='Home'>
+        <div className='Home__Header'>
+          <span className='Home__Header-bg '>hello</span>
+          <section className='Home__Section'>
+            <div className='Home__Section-content'>
+              <p className='Home__Section-paragraph'>
+                I am a Front-End Developer, and welcome to my site
+              </p>
+            </div>
+            <div>
+              <button className='btn'>
+                <Link to='/projects'>
+                  <span className='btn-span'>projects</span>
+                </Link>
+              </button>
+              <button className='btn'>
+                <Link to='/skills'>
+                  <span className='btn-span'>skills</span>
+                </Link>
+              </button>
+              <button className='btn'>
+                <Link to='/about'>
+                  <span className='btn-span'>about me</span>
+                </Link>
+              </button>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
 }
+
+export default Home;
