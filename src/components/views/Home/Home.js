6ed6d03e77871a4b0aedf9cd9cfad6980fa4af0e
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-19 17:37:51 +0100

    add button styles

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -12,7 +12,8 @@ class Home extends Component {
           <section className='Home__Section'>
             <div className='Home__Section-content'>
               <p className='Home__Section-paragraph'>
-                I am a Front-End Developer, and welcome to my site
+                I am a <strong>Front-End Developer</strong>, and welcome to my
+                website.
               </p>
             </div>
             <div>
