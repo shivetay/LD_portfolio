8b4573d2e1b9ff2f165590b545130c9f5e3bb19b
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-20 18:30:03 +0100

    add grid for skills section

--- src/components/views/Skills/Skills.js
+++ src/components/views/Skills/Skills.js
@@ -7,7 +7,12 @@ const Skills = () => {
     <section className='Skills'>
       <div className='Skills__Container'>
         <span className='Skills-bg'>skills</span>
-        <div className='Skills__Content'>ikony</div>
+        <div className='Skills__Content grid'>
+          <div className='Skills__Content-front'>Front-End</div>
+          <div className='Skills__Content-back'>Back-End</div>
+          <div className='Skills__Content-other'>other</div>
+          <div className='Skills__Content-learn'>Learning</div>
+        </div>
       </div>
     </section>
   );
