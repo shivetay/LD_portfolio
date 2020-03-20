f978787c85f2302876fa389da7bf7aaff4aa3144
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-20 09:13:24 +0100

    add file structure for about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
+import About from './components/views/About/About';
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
       <MainLayout>
         <Switch>
           <Route exact path='/' component={Home} />
-          {/* <Route exact path='/projects' component={Projects} />
-          <Route exact path='/skills' component={Skills} />
+          {/* <Route exact path='/projects' component={Projects} /> */}
+          {/* <Route exact path='/skills' component={Skills} /> */}
           <Route exact path='/about' component={About} />
-          <Route exact path='/contact' component={Contact} /> */}
+          {/* <Route exact path='/contact' component={Contact} /> */}
         </Switch>
       </MainLayout>
     </BrowserRouter>
