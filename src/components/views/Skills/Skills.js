5cc12a7998cee74dd39d549a8bb9408717b2e2c3
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-20 12:47:48 +0100

    add basic structure

---
+++ src/components/views/Skills/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Skills.scss';
+
+const Skills = () => {
+  return (
+    <section className='Skills'>
+      <div className='Skills__Container'>
+        <span className='Skills-bg'>skills</span>
+        <div className='Skills__Content'>ikony</div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
