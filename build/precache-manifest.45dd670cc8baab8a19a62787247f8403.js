ffec63350ce17533cc7d19632fbeb1e6ba616d52
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-22 16:05:00 +0100

    commit

---
+++ build/precache-manifest.45dd670cc8baab8a19a62787247f8403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117f2495b7a8bd03aea84f0fae80fc7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4ad41022a03cd665f3e",
+    "url": "/static/css/main.094ee5dc.chunk.css"
+  },
+  {
+    "revision": "571fdd68ff8b53cd77e0",
+    "url": "/static/js/2.eae25a57.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.eae25a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ad41022a03cd665f3e",
+    "url": "/static/js/main.3c188f08.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "5ca8c505c8dcbf9ee23ba176c86c9734",
+    "url": "/static/media/ld_photo.5ca8c505.JPG"
+  }
+]);
