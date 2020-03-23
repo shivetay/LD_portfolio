301c9cedea1e2e8ad9ab15e14685c4f0936b50f3
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-23 15:55:16 +0100

    create project photos

--- src/components/views/Projects/Projects.js
+++ src/components/views/Projects/Projects.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import './Projects.scss';
+
 const Projects = () => {
   return (
-    <div>
-      <h1> Work in progress</h1>
-    </div>
+    <section className='Projects'>
+      <div className='Projects__Container'>
+        <span className='Projects-bg'>projects</span>
+        <div className='Projects__Content'>
+          <h1>Project 1</h1>
+        </div>
+      </div>
+    </section>
   );
 };
 
