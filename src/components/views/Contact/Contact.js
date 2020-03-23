79b5e926674014b538601db779f2306e9b880a69
Łukasz Dawidowicz <lddev@outlook.com>
2020-03-23 09:23:31 +0100

    add basic contet structure

--- src/components/views/Contact/Contact.js
+++ src/components/views/Contact/Contact.js
@@ -2,11 +2,59 @@ import React from 'react';
 
 import './Contact.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faDiceFive,
+  faEnvelope,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons';
+
 const Contact = () => {
   return (
-    <div>
-      <h1>Work in progress</h1>
-    </div>
+    <section className='Contact'>
+      <div className='Contact__Container'>
+        <span className='Contact-bg'>contact</span>
+        <div className='Contact__Content'>
+          <div className='Contact__Content-social'>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.linkedin.com/in/lukaszdawidowicz/'>
+              <FontAwesomeIcon icon={faLinkedin} />
+              linkedin
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://github.com/shivetay'>
+              <FontAwesomeIcon icon={faGithub} />
+              github
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.fiverr.com/shivetay?public_mode=true'>
+              <FontAwesomeIcon icon={faDiceFive} />
+              fiver
+            </a>
+          </div>
+          <div className='Contact__Content-rest'>
+            <span>
+              dawidowicz.lukasz@hotmail.com
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://1drv.ms/b/s!AmWwIHoCT8_JkFtau5aJrAbGM-hi?e=ZTyWtL'>
+              <FontAwesomeIcon icon={faFilePdf} />
+              CV
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
